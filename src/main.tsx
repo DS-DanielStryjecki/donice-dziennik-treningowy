7460bbf89ddf2ff24c6449b24cf69cc520257a0e
DS-DanielStryjecki <poczta@danielstryjecki.pl>
2026-07-12 09:19:29 +0200

    Apply black gray red brand theme and fix exercise artwork

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,2 @@
-import React from 'react';import{createRoot}from'react-dom/client';import{registerSW}from'virtual:pwa-register';import App from './App';import './styles.css';import './additions.css';import './art.css';
+import React from 'react';import{createRoot}from'react-dom/client';import{registerSW}from'virtual:pwa-register';import App from './App';import './styles.css';import './additions.css';import './art.css';import './theme-red.css';
 registerSW({immediate:true});createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
